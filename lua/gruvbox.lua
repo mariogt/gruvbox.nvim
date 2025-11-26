a9ac5cd832e5f95ad47b2f0d47d13ba2e4d093b4
MarioGT <mario@mariogt.com>
2025-11-26 09:54:27 -0300

    darker light background

--- lua/gruvbox.lua
+++ lua/gruvbox.lua
@@ -174,7 +174,7 @@ local function get_colors()
       visual_color = p.dark_rose,
     },
     light = {
-      bg0 = p.light0_soft,
+      bg0 = p.light2,
       bg1 = p.light1,
       bg2 = p.light2,
       bg3 = p.light3,
